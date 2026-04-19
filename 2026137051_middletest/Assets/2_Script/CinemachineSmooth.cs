a40cdaac727cb21e9061cb2c3e242817a31b7594
tteokbokki07-jpg <tteokbokki07@gmail.com>
2026-04-20 01:10:45 +0900

    플레이어 카메라 움직임 개선, 카메라 무브 추가, 스테이지 배경 구분

---
+++ 2026137051_middletest/Assets/2_Script/CinemachineSmooth.cs
@@ -0,0 +1,51 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+public class CinemachineSmooth : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera cam;
+    private CinemachineFollow follow;
+
+    [Header("Offset")]
+    [SerializeField] private Vector3 defaultOffset = new Vector3(0f, 0.4f, -10f);
+    [SerializeField] private Vector3 LeftmoveOffset = new Vector3(1f, 0.4f, -10f);
+    [SerializeField] private Vector3 RightmoveOffset = new Vector3(-1f, 0.4f, -10f);
+
+    [Header("Smooth")]
+    [SerializeField] private float smoothSpeed = 5f;
+
+    private Vector3 velocity;
+
+    void Awake()
+    {
+        follow = cam.GetComponent<CinemachineFollow>();
+    }
+
+    void Update()
+    {
+        if (follow == null) return;
+
+        Vector3 targetOffset = defaultOffset;
+
+        // 좌/우 화살표 입력에 따라 목표 오프셋 결정
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            targetOffset = LeftmoveOffset;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            targetOffset = RightmoveOffset;
+        }
+        else
+        {
+            targetOffset = defaultOffset;
+        }
+
+        follow.FollowOffset = Vector3.SmoothDamp(
+            follow.FollowOffset,
+            targetOffset,
+            ref velocity,
+            1f / smoothSpeed
+        );
+    }
+}
